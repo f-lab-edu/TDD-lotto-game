58db20db94f2a4bec7e84a679d24a4e998efdefa
LeeTaeHeon <forever296@naver.com>
2024-09-14 16:57:11 +0900

    test: Lotto 콘솔 테스트코드 및 Lotto 도메인 작성

--- src/Input.js
+++ src/Input.js
@@ -2,9 +2,9 @@ const Input = () => {
     return `
       <div>
         <p>구입할 금액을 입력해주세요.</p>
-        <form>
-          <input type="text" id="amount" placeholder="금액 입력" />
-          <button>확인</button>
+        <form id="lottoForm">
+          <input type="number" id="amount" placeholder="금액 입력" />
+          <button type="submit">확인</button>
         </form>
       </div>
     `;
