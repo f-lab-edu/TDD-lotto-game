93c8d621887d927ff8cca1060e8b27a9ff747562
LeeTaeHeon <forever296@naver.com>
2024-09-15 11:30:05 +0900

    feat: 로또 가격 입력 및 로또 표시

--- src/Input.js
+++ src/Input.js
@@ -3,7 +3,7 @@ const Input = () => {
       <div>
         <p>구입할 금액을 입력해주세요.</p>
         <form id="lottoForm">
-          <input type="number" id="amount" placeholder="금액 입력" />
+          <input type="text" id="amount" placeholder="금액 입력" />
           <button type="submit">확인</button>
         </form>
       </div>
